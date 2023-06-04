68f8dc99b140093815c6796cf573dc35f6388421
Vichitra Attri <mevichitra@gmail.com>
2023-06-04 07:43:13 +0530

    Add MongoDB

--- app.js
+++ app.js
@@ -1,34 +1,101 @@
-//Require express and body-parser
-const express = require('express');
-const bodyParser = require('body-parser');
-const date = require(__dirname + '/date');
+/* ⚪️ REQUIRE STARTS >> */
+//require express and body-parser
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date");
+// require mongoose
+const mongoose = require("mongoose");
+/* << REQUIRE ENDS ⚪️ */
 
 //Create app from express
 const app = express();
 
 //Set ejs as view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 //Use bodyParser to parse html form data
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 //Save user inputs in an array
-const items = ["Your tasks are added here"]
+const items = ["Your tasks are added here"];
 
-//Get homepage to display list of tasks
-app.get('/', function (req, res) {
-    const day = date.getDate();
-    res.render('list', { day: day, items: items })
-})
+/* 💾 MONGOOSE STARTS  >> */
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-app.post('/', function (req, res) {
-    const item = req.body.newItem
-    items.push(item)
-    res.redirect('/')
-})
+// Create Mongoose Schema
+const itemsSchema = {
+  name: String,
+};
+
+
+// create mongoose model
+const Item = mongoose.model("Item", itemsSchema);
+
+// create new documents
+const item1 = new Item({
+  name: "Welcome to your todolist!",
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item.",
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item.",
+});
+
+// create array of documents
+const defaultItems = [item1, item2, item3];
+/* << MONGOOSE END 💾 */
+
+/* 🔴 HOMEPAGE >> */
+
+//Get homepage to display list of tasks 🌐
+// app.get("/", function (req, res) {
+//   // Get Current Day
+//   const day = date.getDate();
+
+//   // Render list.ejs and pass in day and items 🌐
+//   res.render("list", { day: day, items: items });
+// });
+
+app.get("/", async function (req, res) {
+    try {
+      // Get Current Day
+      const day = date.getDate();
+  
+      // Find all items in the collection
+      const foundItems = await Item.find({});
+  
+      // Check if no items found
+      if (foundItems.length === 0) {
+        // Insert defaultItems into the collection
+        await Item.insertMany(defaultItems);
+        console.log("Default items inserted successfully.");
+        // Redirect to the home page to display the default items
+        res.redirect("/");
+      } else {
+        // Render list.ejs and pass in day and foundItems
+        res.render("list", { day: day, items: foundItems });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+/* << HOMEPAGE 🔴 */
+
+
+
+app.post("/", function (req, res) {
+  const item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
 
 //Listen on port 3000 for http requests
-app.listen('3000', function () {
-    console.log('Listening on port 3000');
-})
+app.listen("3000", function () {
+  console.log("Listening on port 3000");
+});
